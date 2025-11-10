4cce1b731ef86dd31fe17af1a40626dbbccb8bc3
sirons84 <sirons84@naver.com>
2025-11-11 02:42:12 +0900

    feat: 피그마 디자인 적용 (헤더, 푸터, 메인 폼)

--- components/SubmitButton.js
+++ components/SubmitButton.js
@@ -11,7 +11,7 @@ export default function SubmitButton() {
         /* 1. (!! 수정 !!) 
            src="/images/character-wooli.png" -> src="/images/charac-intro01.svg"
         */
-        src="/images/charac-intro01.svg" 
+        src="/images/character-wooli.svg"
         alt="울리 캐릭터"
         width={300}  // (SVG 원본 크기에 맞게 조절하셔도 됩니다)
         height={300} // (CSS가 실제 크기를 제어합니다)
